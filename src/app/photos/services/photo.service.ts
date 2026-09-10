1cdf6898ceac4a15baf144847714b1e26247afde
konstantin-stepanets <konstantins@revuze.it>
2026-09-10 22:23:51 +0200

    feat: implement PhotoService with delayed piсtures fetch

--- src/app/photos/services/photo.service.ts
+++ src/app/photos/services/photo.service.ts
@@ -1,6 +1,28 @@
 import { Injectable } from '@angular/core';
+import { delay, Observable, of } from 'rxjs';
+import { Photo } from '../../shared/models/photo.model';
 
 @Injectable({
   providedIn: 'root',
 })
-export class PhotoService {}
+export class PhotoService {
+  loadPhotos(count: number): Observable<Photo[]> {
+    const photos = Array.from({ length: count }, () => this.createPhoto());
+    const delayMs = this.randomDelay();
+
+    return of(photos).pipe(delay(delayMs));
+  }
+
+  private createPhoto(): Photo {
+    const id = crypto.randomUUID();
+
+    return {
+      id,
+      url: `https://picsum.photos/seed/${id}/200/300`,
+    };
+  }
+
+  private randomDelay(): number {
+    return Math.floor(Math.random() * 101) + 200;
+  }
+}
